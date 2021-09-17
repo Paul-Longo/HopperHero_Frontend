047d47816ae47cbf9028522dfa508893049b9ec4
Paul-Longo <imanazarite88@gmail.com>
2021-09-17 16:39:45 -0400

    updated the return with login, register, and wodlist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,38 @@
+import axios from 'axios';
 import React, {Component} from 'react';
+import { Login } from './Users/login';
+import { Register } from './Users/register';
+import WodList from './Wod/WodList';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            wod =[],
+    state = {
+            wod: [],
         }
-    }
-
-
+    
     //Display a random workout
     componentDidMount() {
         this.getRandomWod();
     }
 
     //GET request for Wod
-    
+    async getRandomWod() {
+        let response = await axios.get('http://127.0.0.1:8000/api/hopper/all')
+        this.setState({
+            wod: response.data
+        });
+    }
 
 
     render() { 
         return (
-            <h1>Hopper Hero</h1>
+            
+            <div>
+                <h1>Hopper Hero</h1>
+                <Login /> <br />
+                <Register />
+                <WodList wod={this.state.wod} />
+            </div>
+            
         );
     }
 }
