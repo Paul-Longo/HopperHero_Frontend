85c67ee31b6a06192a2e09af4d494a309624ffaf
Paul-Longo <imanazarite88@gmail.com>
2021-09-20 13:59:44 -0400

    updated the user login and register

--- src/components/Users/login.jsx
+++ src/components/Users/login.jsx
@@ -1,33 +1,65 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Button, Form, Container } from 'react-bootstrap'
+import { Redirect } from 'react-router';
 
 
-export class Login extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [redirect, setRedirect] = useState(false)
 
-  render() {
-    return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Login</div>
-        <div className="content">
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
-            </div>
+  const submit = async (e) => {
+    let res
+    e.preventDefault();
+
+    axios.post('http://127.0.0.1:8000/api/auth/login/', {
+      username,
+      password,
+    }).then((response) => {
+      localStorage.setItem('token', response.data.access)
+      window.location.reload()
+    }, (err) => {
+      console.log(err)
+    })
+
+    const content = res
+    console.log(content)
+
+    setRedirect(true)
+    }
+
+    if (redirect) {
+        return <Redirect to='/home'/>
+    }
+
+  return (
+
+    <Container>
+      <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
+
+
+          <Form onSubmit={submit}>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+              <input type="text" className="form-control"  placeholder="Username" required 
+              onChange={e => setUsername(e.target.value)}/>
+              
+              <input type="password" className="form-control"  placeholder="Password" required 
+              onChange={e => setPassword(e.target.value)}/>
+
+            
+            <Button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</Button>
+          </Form>
+
+
           </div>
-        </div>
-        <div className="footer">
-          <button type="button" className="btn">
-            Login
-          </button>
-        </div>
+          <div class="col-md-3"></div>
       </div>
-    );
-  }
+    </Container>
+  )
 }
+
+export default Login;
--- src/components/Users/register.jsx
+++ src/components/Users/register.jsx
@@ -1,45 +1,140 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Button, Form, Container } from 'react-bootstrap'
+import { Redirect } from 'react-router';
 
 
-export class Register extends Component {
-    state ={
-        register: ''
+const Register = () => {
+  const [first_name, setFirstName] = useState('')
+  const [last_name, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [redirect, setRedirect] = useState(false)
+
+
+    const submit = async(e) => {
+      let res
+      e.preventDefault()
+      let user = {
+          username : username,
+          password: password,
+          email: email,
+          first_name: first_name,
+          last_name: last_name,
+      }
+      try{  
+         res = await axios.post('http://127.0.0.1:8000/api/auth/register/', user)
+         
+      }
+      catch(err){
+        console.log(err)
+      }
+    
+      const content = res
+      console.log(content)
+      setRedirect(true);
+
+    }
+    if (redirect) {
+        return <Redirect to='/login' />
     }
 
-  render() {
-    return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Register</div>
-        <div className="content">
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="first_name">First Name</label>
-              <input type="text" name="first_name" placeholder="First Name" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="last_name">Last Name</label>
-              <input type="text" name="last_name" placeholder="Last Name" />
-            </div>
-          </div>
+  return (
+
+    <Container>
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+
+          <Form onSubmit={submit}>
+
+          <h1 className="h3 mb-3 fw-normal">Register here:</h1>
+                <input type="text" className="form-control"  placeholder="First Name" required 
+                onChange={e => setFirstName(e.target.value)}/>
+
+                <input type="text" className="form-control"  placeholder="Last Name" required 
+                onChange={e => setLastName(e.target.value)}/>
+
+                <input type="text" className="form-control"  placeholder="Username" required 
+                onChange={e => setUsername(e.target.value)}/>
+                
+                <input type="password" className="form-control"  placeholder="Password" required 
+                onChange={e => setPassword(e.target.value)}/>
+
+                <input type="email" className="form-control"  placeholder="Email@example.com" required 
+                onChange={e => setEmail(e.target.value)}/>
+
+                <Button className="w-100 btn btn-lg btn-primary" type="submit">Create Account</Button>
+
+          </Form>
+          
         </div>
-        <div className="footer">
-          <button type="button" className="btn">
-            Register
-          </button>
-        </div>
-      </div>
-    );
-  }
+        <div class="col-md-3"></div>
+    </div>
+    </Container>
+
+
+      
+
+  )
 }
+
+export default Register;
+
+// import axios from 'axios';
+// import React, { Component } from 'react';
+
+// class Register extends Component {
+//     constructor(props) {
+//         super(props);
+//         this. state ={
+//             username: "",
+//             password: "",
+//             email: "",
+//             first_name: "",
+//             last_name: ""
+//         }
+//     }
+
+//     async makePostRequest() {
+//         try{
+//             let response = await axios.post('http://127.0.0.1:8000/api/auth/register/')
+//             this.setState({
+//                 username: response.data,
+//                 password: response.data,
+//                 email: response.data,
+//                 first_name: response.data,
+//                 last_name: response.data
+//             })
+//         }
+//         catch(ex) {
+//             console.log('Error in API call')
+//         }
+//     }
+
+//     render() { 
+//         return ( 
+//             <form>
+//                 <lable>Username</lable>
+//                 <input type="text" name="username" onChange={this.handleChange} value={this.state.username} placeholder="Username" />
+
+//                 <lable>Password</lable>
+//                 <input type="text" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" />
+
+//                 <lable>Email</lable>
+//                 <input type="text" name="email" onChange={this.handleChange} value={this.state.email} placeholder="email" />
+
+//                 <lable>First Name</lable>
+//                 <input type="text" name="first_name" onChange={this.handleChange} value={this.state.first_name} placeholder="First Name" />
+
+//                 <lable>Last Name</lable>
+//                 <input type="text" name="last_name" onChange={this.handleChange} value={this.state.last_name} placeholder="Last Name" />
+
+//                 <button onClick={this.handleSubmit}>Submit</button>                       
+//             </form>
+//          );
+//     }
+// }
+
+// export default Register;
