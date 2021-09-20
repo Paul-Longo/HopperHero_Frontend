d0aeaee414147ed286eba9302f9e3995ef5f8912
Paul-Longo <imanazarite88@gmail.com>
2021-09-20 13:59:22 -0400

    created a chart from charts.js

---
+++ src/components/Chart/bar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [
+        {
+            label: 'Fitness Graph',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 2,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+        }
+    ]
+};
+
+function Graph() {
+    return (
+        <div style={{ height: '600px', width: '800px' }}>
+            <Bar
+                data={data}
+                width={100}
+                height={50}
+                options={{
+                    maintainAspectRatio: false
+                }}
+            />
+        </div>
+    );
+}
+export default Graph;
