f1da9f89d9565048e6b3ae28fd230bbfbd11721d
Paul-Longo <imanazarite88@gmail.com>
2021-09-21 08:57:40 -0400

    added an image and h3 tag

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+
 const Home = () => {
     return (
     <div>
-        <center><h1>Welcome to Hopper Hero</h1></center>
+        <center>
+            <h1>Welcome to Hopper Hero</h1> <br />
+            <img className="img" src="./images/OIP.png" alt="" /> <br />
+            <h3>Select a Link from the Nav Bar</h3>
+        </center>
+
     </div> 
     );
 }
