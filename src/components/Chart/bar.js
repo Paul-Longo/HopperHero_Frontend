931f45fe03a8b98c5a909fbd8ee994b709bae306
Paul-Longo <imanazarite88@gmail.com>
2021-09-20 22:49:16 -0400

    updated the lables for the bar graph

--- src/components/Chart/bar.js
+++ src/components/Chart/bar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
 const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    labels: ['Clean', 'Power Clean', 'Clean and Jerk', 'Hang Clean', 'Hang Power Clean', 'Snatch', 'Power Snatch', 'Hang Snatch', 'Hang Power Snatch', 'Muscle Snatch'],
     datasets: [
         {
-            label: 'Fitness Graph',
+            label: '1RM Graph',
             backgroundColor: 'rgba(255,99,132,0.2)',
             borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 2,
+            borderWidth: 3,
             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
             hoverBorderColor: 'rgba(255,99,132,1)',
             data: []
