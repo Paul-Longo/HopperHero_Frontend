4ab149faf956b0ead958bed4ac46f980d7930a51
Paul-Longo <imanazarite88@gmail.com>
2021-09-28 03:31:17 -0400

    updated return routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import axios from 'axios';
 import React, {Component} from 'react';
 import  Login  from './Users/login';
 import  Register  from './Users/register';
 import WodList from './Wod/WodList';
 import NavBar from './NavBar/navbar';
-import Graph from './Chart/bar';
+import Chart from './Chart/bar';
 import Home from './Home/Home';
 import Hopper from './Hopper/Hopper';
+import Profile from './Profile/Profile';
 import "./App.css"
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -14,28 +14,6 @@ class App extends Component {
     state = {
             wod: [],
         }
-    
-    //Display a random workout
-    componentDidMount() {
-        this.getRandomWod();
-    }
-
-    //GET request for Wod
-    async getRandomWod() {
-        let response = await axios.get('http://127.0.0.1:8000/api/hopper/all')
-        this.setState({
-            wod: response.data
-        });
-    }
-
-    createUser = (newUser) => {
-        this.state.users.push(newUser);
-        axios.post ('http://127.0.0.1:8000/api/auth/register/', newUser)
-            .then(response => this.setState = ({
-                newUser: response.data
-            }));
-        window.location.reload();
-    }
 
 
     render() { 
@@ -46,11 +24,12 @@ class App extends Component {
                    <NavBar />
                     <Switch className={App}>
                         <Route exact path="/" component={Home} />
-                        <Route exact path="/Graph" component={Graph} />
-                        <Route exact path='/Login' component={Login} />
+                        <Route exact path="/Chart" component={Chart} />
+                        <Route exact path='/Login' component={Login}/>
                         <Route exact path='/Register' component={Register} />
                         <Route exact path='/WodList' component={WodList} />
                         <Route exact path='/Hopper' component={Hopper} />
+                        <Route exact path='/Profile' component={Profile} />
                     </Switch>
                 </BrowserRouter>
             </div>
