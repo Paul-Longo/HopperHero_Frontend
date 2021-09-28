9b01a4897f4f7c192a87b1447758e60a00c1e11a
Paul-Longo <imanazarite88@gmail.com>
2021-09-28 03:34:20 -0400

    created a randomizer in hopper.jsx

--- src/components/Hopper/Hopper.jsx
+++ src/components/Hopper/Hopper.jsx
@@ -1,44 +1,72 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import "./Hopper.css"
+import "./Hopper.css";
+import GetWod from "./GetWod"
+
+let wod = []
 
 class Hopper extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            wod: []
+        this.state = {
+            rwods: [],
         }
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    componentDidMount() {
-        this.getWod();
+    componentWillMount() {
+        this.GetWod();
+    }    
+
+    async GetWod() {
+        let response = await axios.get('http://127.0.0.1:8000/api/hopper/all/')
+        wod = response.data
+        console.log(response.data)
     }
 
-    async getWod() {
-        let response = await axios.get('http://127.0.0.1:8000/api/hopper/all')
+
+    handleClick = () =>{
+        let arr = []
+        for (let i = 0; i < 3; i++){
+            arr.push(Math.floor(Math.random() * 50))
+        }
+        let w1 = wod[arr[0]]
+        let w2 = wod[arr[1]]
+        let w3 = wod[arr[2]]
+        console.log(arr)
         this.setState({
-            wod: response.data
-        });
+            rwods: [w1, w2, w3]
+        })
+        this.forceUpdate();
     }
-    
+
     render() {
-        return (
-            <div>
-                <h1 className="header">Random Wods</h1>
-                <table className='hopper'>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                    </tr>
-                    {this.state.wod.map((wod, index)=>
-                        <tr key={index}>
-                            <td className='hopper__name'>{wod.name}</td>
-                            <td className='hopper__description'>{wod.description}</td>
-                        </tr>)}
-                </table>
-                <button className="btn" onClick={this.state.wod}>LETS GO!</button>
-            </div>
-        )
+        if(this.state.rwods.length > 0) {
+            let wodlist = this.state.rwods
+            return (
+                <div>
+                    <h1 className="header">Random Wods</h1>
+                    <table className='hopper'>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>  
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {wodlist.map((wod, i)=> (
+                            <GetWod key={i} wod={wod}/>
+                        ))}
+                        </tbody>
+                    </table>
+                    <center><button className="btn" onClick={() => this.handleClick()}>LETS GO!</button></center>
+                </div>
+            )
+        }
+        else{
+            return(<div><center><button className="btn" onClick={() => this.handleClick()}>LETS GO!</button></center></div>)
+           
+        }
     }
 }
 
