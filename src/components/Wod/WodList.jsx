66cfbbe8f51c61a0faaacab912572b2b30794bb9
Paul-Longo <imanazarite88@gmail.com>
2021-10-01 17:59:13 -0400

    added user and likes to state, added async get likes, added thead and tbody tags

--- src/components/Wod/WodList.jsx
+++ src/components/Wod/WodList.jsx
@@ -9,11 +9,14 @@ class WodList extends Component {
         super(props);
         this.state = { 
             wod: [],
+            user: this.props.user,
+            likes: []
         }
     }
 
     componentDidMount() {
         this.getWod();
+        this.getLikes();
     }
 
     async getWod() {
@@ -23,19 +26,31 @@ class WodList extends Component {
         });
     }
 
+    async getLikes() {
+        let response = await axios.get('http://127.0.0.1:8000/api/hopper/likes/user/' + this.state.user.id)
+        await this.setState({
+           likes: response.data
+        });
+        console.log(this.state.likes)
+    }
+
     render() {
         return (
             <div>
                 <h1 className="header">Hero Wods</h1>
                 <Button className='likes' to='src/components/Favorites/likespage.jsx' >Likes</Button>
                 <table className='wod-list'>
+                    <thead>
                     <tr>
                         <th>Name</th>
                         <th>Description</th>
                     </tr>
+                    </thead>
+                    <tbody>
                     {this.state.wod.map((wod, index)=>
-                    <WodListItems wod={wod} index={index} />
+                    <WodListItems wod={wod} key={index} />
                         )}
+                    </tbody>
                 </table>
                 
             </div>
