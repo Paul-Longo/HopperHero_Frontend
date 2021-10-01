8d7c10d68d9316358d7a4258e92442798c5ee1b8
Paul-Longo <imanazarite88@gmail.com>
2021-10-01 09:15:37 -0400

    created a like button for each item in the wodlist table

---
+++ src/components/Wod/WodListItems.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+class WodListItems extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wod: [],
+            liked: false,
+        }
+    }
+
+    toggleLike = () => {
+        this.setState({
+            liked: !this.state.liked
+        })
+    }
+
+    render() {
+        const changeColor = this.state.liked ? "red" : "grey"
+
+        return (                   
+            <tr key={this.props.index}>
+                <td className='wod-list__name'>{this.props.wod.name}</td>
+                <td className='wod-list__description'>{this.props.wod.description}</td>
+                <button className="likebtn" onClick={this.toggleLike}>
+                    <i className="fas fa-heart fa-lg" style={{ color: changeColor }}></i>
+                </button>
+            </tr>
+        )
+    }
+}
+ 
+export default WodListItems;
