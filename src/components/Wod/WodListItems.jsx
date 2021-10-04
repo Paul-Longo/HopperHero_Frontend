0be1dfc8546a30964bc72375fd23d32e25861399
Paul-Longo <imanazarite88@gmail.com>
2021-10-04 10:58:22 -0400

    updated functionality of the like button.

--- src/components/Wod/WodListItems.jsx
+++ src/components/Wod/WodListItems.jsx
@@ -1,16 +1,59 @@
+import axios from "axios";
 import React from "react";
-
 
 class WodListItems extends React.Component {
     constructor(props) {
         super(props);
+        let likeStatus = false
+        this.props.likes.forEach((like) => {
+            if(this.props.wod.id === like.wod) {
+                likeStatus = true
+            }
+        })
         this.state = {
             wod: [],
-            liked: false,
+            user: this.props.user,
+            liked: likeStatus,
+            likes: this.props.likes
         }
+        console.log(this.state.likes)
+    }
+
+    componentWillMount() {
+        // this.getLikes();
+        // this.state.likes.forEach((like) => {
+        //     if(this.props.wod.id === like.wod) {
+        //         this.setState({
+        //             liked: true
+        //         })
+        //     }
+        // })
+    }
+
+    async getLikes() {
+        let response = await axios.get('http://127.0.0.1:8000/api/hopper/likes/user/' + this.state.user.id)
+        this.setState({
+           likes: response.data
+        });
     }
 
     toggleLike = () => {
+
+        if(this.state.liked === false){
+            let like = {
+                user: this.props.user.id,
+                wod: this.props.wod.id
+            }
+            let response = axios.post('http://127.0.0.1:8000/api/hopper/likes/', like)
+        }
+        else{
+            this.state.likes.forEach((like) => {
+                if(this.props.wod.id === like.wod) {
+                    let response = axios.delete('http://127.0.0.1:8000/api/hopper/likes/' + like.id)
+                    console.log(like.id)
+                }
+            })
+        }
         this.setState({
             liked: !this.state.liked
         })
