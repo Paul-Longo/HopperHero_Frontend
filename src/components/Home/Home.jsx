3826b9bf017a9bc13eeee52d7e8c8c581d66f445
Paul-Longo <imanazarite88@gmail.com>
2021-10-04 10:54:30 -0400

    converted home page to a class

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-
-const Home = () => {
-    return (
-    <div>
-        <center>
-            <h1>Welcome to Hopper Hero</h1> <br />
-            <img className="img" src="./images/OIP.png" alt="" /> <br />
-            <h3>Select a Link from the Nav Bar</h3>
-        </center>
-
-    </div> 
-    );
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return (     
+            <div>
+                <center>
+                    <h1>Welcome to Hopper Hero</h1> <br />
+                    <img className="img" src="./images/OIP.png" alt="" /> <br />
+                    <h3>Select a Link from the Nav Bar</h3>
+                </center>
+            </div> 
+        );
+    }
 }
  
 export default Home;
